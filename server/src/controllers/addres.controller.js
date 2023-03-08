5ed32c17b3b4aaa0a3dfa3da7ceb4d8d82829549
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-03-07 23:15:17 -0600

    quitando las validaciones para evitar la duplicacion de titles

--- server/src/controllers/addres.controller.js
+++ server/src/controllers/addres.controller.js
@@ -5,15 +5,8 @@ const tryCatch = require('../utils/tryCatch')
 const modelAddres = require('../models/addressModels')
 
 exports.addAdrres = tryCatch( async(req, res, next) => {
-  console.log('currentUser -> ', req)
+  
   const idUser = req.currentUser.id
-  const { title } = req.body
-
-  const existAddress = await modelAddres.findOne({idUser, title})
-  // console.log('addrees -> ', existAddress)
-  if (existAddress !== null) {
-    return next( new AppError('Este nombre ya lo tiene registrado en sus direcciones', 404))
-  }
 
   const newAddress = new modelAddres({
     ...req.body,
